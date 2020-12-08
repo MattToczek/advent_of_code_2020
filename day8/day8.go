4a27b5da1dc88e8fb87dd07686928cd076e8c88e
MattToczek <toczek.matt@gmail.com>
2020-12-08 12:45:04 +0000

    refactor

--- day8/day8.go
+++ day8/day8.go
@@ -50,12 +50,8 @@ func contains(s []int, e int) bool {
 	return false
 }
 
-func do_action(action_object_map map[int]action_object, key int, ind_so_far []int, count int) int {
-	if contains(ind_so_far, key) {
-		return count
-	}
+func do_action(action_object_map map[int]action_object, key int, count int) []int {
 	var next_key int
-	ind_so_far = append(ind_so_far, key)
 	switch action_object_map[key].action {
 	case "jmp":
 		next_key = key + action_object_map[key].number
@@ -65,7 +61,17 @@ func do_action(action_object_map map[int]action_object, key int, ind_so_far []in
 		next_key = key + 1
 		count += action_object_map[key].number
 	}
-	return do_action(action_object_map, next_key, ind_so_far, count)
+	return []int{count, next_key}
+}
+
+func do_action_recursive(action_object_map map[int]action_object, key int, ind_so_far []int, count int) int {
+	if contains(ind_so_far, key) {
+		return count
+	}
+	slice := do_action(action_object_map, key, count)
+	ind_so_far = append(ind_so_far, key)
+	count = slice[0]
+	return do_action_recursive(action_object_map, slice[1], ind_so_far, count)
 }
 
 func check_for_no_loop(action_object_map map[int]action_object, key int, ind_so_far []int, count int) bool {
@@ -74,49 +80,27 @@ func check_for_no_loop(action_object_map map[int]action_object, key int, ind_so_
 	} else if contains(ind_so_far, key) {
 		return false
 	}
-	var next_key int
+	slice := do_action(action_object_map, key, count)
 	ind_so_far = append(ind_so_far, key)
-	switch action_object_map[key].action {
-	case "jmp":
-		next_key = key + action_object_map[key].number
-	case "nop":
-		next_key = key + 1
-	case "acc":
-		next_key = key + 1
-		count += action_object_map[key].number
-	}
-	return check_for_no_loop(action_object_map, next_key, ind_so_far, count)
+	count = slice[0]
+	return check_for_no_loop(action_object_map, slice[1], ind_so_far, count)
 }
 
 func check_map_for_no_loop(action_object_map map[int]action_object) int {
 	for key, value := range action_object_map {
 		if value.action == "jmp" {
-			new_map := make(map[int]action_object)
-			for k, v := range action_object_map {
-				new_map[k] = v
-			}
-			var new_ao action_object
-			new_ao.action = "nop"
-			new_ao.number = value.number
-			new_map[key] = new_ao
+			new_map := change_action(action_object_map, key, "nop")
 			var empty_slice []int
 			if check_for_no_loop(new_map, 0, empty_slice, 0) {
 				var empty_slice []int
-				return do_action(new_map, 0, empty_slice, 0)
+				return do_action_recursive(new_map, 0, empty_slice, 0)
 			}
 		} else if value.action == "nop" {
-			new_map := make(map[int]action_object)
-			for k, v := range action_object_map {
-				new_map[k] = v
-			}
-			var new_ao action_object
-			new_ao.action = "jmp"
-			new_ao.number = value.number
-			new_map[key] = new_ao
+			new_map := change_action(action_object_map, key, "jmp")
 			var empty_slice []int
 			if check_for_no_loop(new_map, 0, empty_slice, 0) {
 				var empty_slice []int
-				return do_action(new_map, 0, empty_slice, 0)
+				return do_action_recursive(new_map, 0, empty_slice, 0)
 			}
 		}
 	}
@@ -124,21 +108,24 @@ func check_map_for_no_loop(action_object_map map[int]action_object) int {
 }
 
 func change_action(action_object_map map[int]action_object, key int, action string) map[int]action_object {
-	var ao action_object
-	ao.action = action
-	ao.number = key + 1
-	action_object_map[key] = ao
-	return action_object_map
+	new_map := make(map[int]action_object)
+	for k, v := range action_object_map {
+		new_map[k] = v
+	}
+	var new_ao action_object
+	new_ao.action = action
+	new_ao.number = action_object_map[key].number
+	new_map[key] = new_ao
+	return new_map
 }
 
 func main() {
 	input := get_input_slice("day8_input.txt")
 	action_object_map := split_to_map(input)
 	var empty_slice []int
-	fmt.Println(do_action(action_object_map, 0, empty_slice, 0))
+	fmt.Println(do_action_recursive(action_object_map, 0, empty_slice, 0))
 
 	fmt.Println(check_map_for_no_loop(action_object_map))
-
 }
 
 type action_object struct {
